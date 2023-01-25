42ab469b9316c49fe486249c712293c24bee2dcd
Butcherok <butcherok231@gmail.com>
2023-01-25 16:08:06 +0200

    fix task 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,11 +1,10 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
-
 // Описаний в документації
 import SimpleLightbox from "simplelightbox";
 // Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
 
 const imagesContainer = document.querySelector('.gallery');
 
