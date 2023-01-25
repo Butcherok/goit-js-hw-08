cfe80e9cf51d1e3814c7cf5ac1a7da04ac809f84
Butcherok <butcherok231@gmail.com>
2023-01-25 16:06:28 +0200

    task 01 - done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,34 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const imagesContainer = document.querySelector('.gallery');
+
+function createImageGallery(galleryItems) {
+    return galleryItems
+    .map(({preview, original, description}) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+            />
+        </a>
+        `;
+    })
+    .join('');
+};
+
+const imagesMarkup = createImageGallery(galleryItems);
+imagesContainer.insertAdjacentHTML('beforeend', imagesMarkup);
+
+new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
 console.log(galleryItems);
