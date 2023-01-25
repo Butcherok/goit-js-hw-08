d263cfba54c9de1f282e4aa9c2eb817152f2b060
Butcherok <butcherok231@gmail.com>
2023-01-25 23:10:08 +0200

    get currTime

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function() {
+    console.log('played the video!');
+
+});
+
+player.on('timeupdate', function(currTime) {
+    console.log(currTime.seconds);
+
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
 
